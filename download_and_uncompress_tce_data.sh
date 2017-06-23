3aeb1e9857d8dbd27c5aaf2946432829ca5447c9
Marcus Carvalho <marcuswac@gmail.com>
2017-06-23 10:36:04 -0300

    script to download data from TCE site

---
+++ download_and_uncompress_tce_data.sh
@@ -0,0 +1,9 @@
+cd data/
+wget https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Licitacao_Esfera_Municipal.txt.gz
+gzip -d TCE-PB-SAGRES-Licitacao_Esfera_Municipal.txt.gz
+
+wget https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Participantes_Licitacao_Esfera_Municipal.txt.gz
+gzip -d TCE-PB-SAGRES-Participantes_Licitacao_Esfera_Municipal.txt.gz
+
+wget https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Propostas_Licitacao_Esfera_Municipal.txt.gz
+gzip -d TCE-PB-SAGRES-Propostas_Licitacao_Esfera_Municipal.txt.gz
