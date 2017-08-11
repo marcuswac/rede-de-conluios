e36a445122e6f3e8744fe4285e60779716f686db
Marcus Carvalho <marcuswac@gmail.com>
2017-08-11 16:20:08 -0300

    fix

--- api_server.R
+++ api_server.R
@@ -2,8 +2,7 @@ library(plumber)
 
 argv <- commandArgs(TRUE)
 
-server_port <- ifelse(is.na(argv[1]), 3839, argv[1]) %>%
-    as.integer()
+server_port <- ifelse(is.na(argv[1]), 3839, as.integer(argv[1]))
 
 r <- plumb("R/conluios_api.R")
 r$run(port=server_port)
