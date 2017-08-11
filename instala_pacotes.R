5cc699cebad7d5a126c2226acff31a11cc507f72
Marcus Carvalho <marcuswac@gmail.com>
2017-08-11 15:56:57 -0300

    virgula

--- instala_pacotes.R
+++ instala_pacotes.R
@@ -1,5 +1,5 @@
 #! /usr/local/bin/Rscript
 
-pkgs <- c("tidyverse", "DT", "htmlwidgets", "networkD3", "plumber" "shiny",
+pkgs <- c("tidyverse", "DT", "htmlwidgets", "networkD3", "plumber", "shiny",
           "shinythemes", "shinydashboard", "shinyBS", "shinyjs", "V8")
 install.packages(pkgs, dependencies = TRUE, repos = "http://cran.rstudio.com/")
