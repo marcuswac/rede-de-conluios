9eaccb2c2d0bc4179da1b83a2dd490bcf6c5acda
Marcus Carvalho <marcuswac@gmail.com>
2017-08-22 15:11:19 -0300

    Fix df de socios não carregado.

--- R/conluios_api.R
+++ R/conluios_api.R
@@ -4,6 +4,7 @@ library(plumber)
 source("R/common.R")
 
 coparticipacoes <- suppressMessages(carrega_dados_coparticipacoes())
+
 participantes_stats <- carrega_dados_participantes_stats_com_cnae() %>%
   filter(nu_cpfcnpj %in% coparticipacoes$nu_cpfcnpj_1 |
            nu_cpfcnpj %in% coparticipacoes$nu_cpfcnpj_2) %>%
@@ -18,33 +19,9 @@ participantes_stats <- carrega_dados_participantes_stats_com_cnae() %>%
   select(nu_cpfcnpj, nome, n_licitacoes, n_vencedora, tipo_sancao, idoneidade,
          secao_cnae, subclasse_cnae)
 
-# get_coparticipantes <- function(participante_cnpj, participantes_stats,
-#                                 coparticipacoes) {
-#   if (is.null(participante_cnpj) || participante_cnpj == "") {
-#     return(data.frame())
-#   }
-#   participante <- participantes_stats %>%
-#     filter(nu_cpfcnpj == participante_cnpj)
-#   
-#   coparticipacoes_filt <- coparticipacoes %>%
-#     filter(nu_cpfcnpj_1 == participante_cnpj |
-#              nu_cpfcnpj_2 == participante_cnpj) %>%
-#     transmute(nu_cpfcnpj = ifelse(nu_cpfcnpj_1 != participante_cnpj,
-#                                   nu_cpfcnpj_1, nu_cpfcnpj_2),
-#               n_coparticipacoes = frequency)
-#   
-#   if (nrow(coparticipacoes_filt) == 0) {
-#     return(data.frame())
-#   }
-#   
-#   coparticipacoes <- coparticipacoes_filt %>%
-#     left_join(participantes_stats, by = "nu_cpfcnpj") %>%
-#     arrange(desc(n_coparticipacoes)) %>%
-#     ungroup() %>%
-#     select(nome, nu_cpfcnpj, n_coparticipacoes, n_licitacoes, n_vencedora,
-#            n_mesmo_socio, tipo_sancao)
-#   coparticipacoes
-# }
+socios <- carrega_dados_socios_pb() %>%
+  select(nu_cpfcnpj, socio_nome, socio_nome_legal) %>%
+  filter(!is.na(socio_nome))
 
 #* Retorna coparticipacoes de empresas em licitacoes
 #* @get /coparticipacoes
